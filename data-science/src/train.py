e55321897b2580107d335c44995170cc821c4edc
Swati Baikar <32398290+swatibaikar01@users.noreply.github.com>
2025-02-21 08:51:11 -0600

    Update train.py

--- data-science/src/train.py
+++ data-science/src/train.py
@@ -16,12 +16,12 @@ def parse_args():
     '''Parse input arguments'''
 
     parser = argparse.ArgumentParser("train")
-    parser.add_argument("--train_data", type=_______, help="Path to train dataset")  # Specify the type for train_data
-    parser.add_argument("--test_data", type=_______, help="Path to test dataset")  # Specify the type for test_data
-    parser.add_argument("--model_output", type=_______, help="Path of output model")  # Specify the type for model_output
-    parser.add_argument('--n_estimators', type=_______, default=_____,
+    parser.add_argument("--train_data", type=str, help="Path to train dataset")  # Specify the type for train_data
+    parser.add_argument("--test_data", type=str, help="Path to test dataset")  # Specify the type for test_data
+    parser.add_argument("--model_output", type=str, help="Path of output model")  # Specify the type for model_output
+    parser.add_argument('--n_estimators', type= int, default=100,
                         help='The number of trees in the forest')  # Specify the type and default value for n_estimators
-    parser.add_argument('--max_depth', type=_______, default=_______,
+    parser.add_argument('--max_depth', type=int, default=None,
                         help='The maximum depth of the tree')  # Specify the type and default value for max_depth
 
     args = parser.parse_args()
@@ -32,26 +32,26 @@ def main(args):
     '''Read train and test datasets, train model, evaluate model, save trained model'''
 
     # Read train and test data from _______
-    train_df = pd.________(Path(args.train_data) / "______.csv")
-    test_df = pd.________(Path(args.test_data) / "______.csv")
+    train_df = pd.read_csv(Path(args.train_data) / "train_data.csv")
+    test_df = pd.read_csv(Path(args.test_data) / "test_data.csv")
 
     # Split the data into ______(X) and ______(y) 
-    y_train = train_df['______']  # Specify the target column
-    X_train = train_df.drop(columns=['______'])
-    y_test = test_df['______']
-    X_test = test_df.drop(columns=['______'])
+    y_train = train_df['Price']  # Specify the target column
+    X_train = train_df.drop(columns=['Price'])
+    y_test = test_df['Price']
+    X_test = test_df.drop(columns=['Price'])
 
     # Initialize and train a RandomForest Regressor
-    model = RandomForestRegressor(n_estimators=args.n_estimators, max_depth=args.______, random_state=42)  # Provide the arguments for RandomForestRegressor
-    model.________(X_train, y_train)  # Train the model
+    model = RandomForestRegressor(n_estimators=args.n_estimators, max_depth=args.max_depth, random_state=42)  # Provide the arguments for RandomForestRegressor
+    model.fit(X_train, y_train)  # Train the model
 
     # Log model hyperparameters
-    mlflow.log_param("model", "_________")  # Provide the model name
+    mlflow.log_param("model", "RandomForestRegressor")  # Provide the model name
     mlflow.log_param("n_estimators", args.n_estimators)
-    mlflow.log_param("max_depth", args.______)
+    mlflow.log_param("max_depth", args.max_depth)
 
     # Predict using the RandomForest Regressor on test data
-    yhat_test = model._______(X_test)  # Predict the test data
+    yhat_test = model.predict(X_test)  # Predict the test data
 
     # Compute and log mean squared error for test data
     mse = mean_squared_error(y_test, yhat_test)
@@ -59,7 +59,7 @@ def main(args):
     mlflow.log_metric("MSE", float(mse))  # Log the MSE
 
     # Save the model
-    mlflow.sklearn.________(sk_model=model, path=args.model_output)  # Save the model
+    mlflow.sklearn.save_model(sk_model=model, path=args.model_output)  # Save the model
 
 if __name__ == "__main__":
     
