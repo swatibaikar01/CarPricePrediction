65e5e37e8789b07953ab6c1d351c0f8836e7f1b1
Swati Baikar <32398290+swatibaikar01@users.noreply.github.com>
2025-02-21 08:37:27 -0600

    Update register.py
    updated register.py

--- data-science/src/register.py
+++ data-science/src/register.py
@@ -14,9 +14,9 @@ def parse_args():
     '''Parse input arguments'''
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', type=_____, help='Name under which model will be registered')  # Hint: Specify the type for model_name (str)
-    parser.add_argument('--model_path', type=_____, help='Model directory')  # Hint: Specify the type for model_path (str)
-    parser.add_argument("--model_info_output_path", type=_____, help="Path to write model info JSON")  # Hint: Specify the type for model_info_output_path (str)
+    parser.add_argument('--model_name', type=str, help='Name under which model will be registered')  # Hint: Specify the type for model_name (str)
+    parser.add_argument('--model_path', type=str, help='Model directory')  # Hint: Specify the type for model_path (str)
+    parser.add_argument("--model_info_output_path", type=str, help="Path to write model info JSON")  # Hint: Specify the type for model_info_output_path (str)
     args, _ = parser.parse_known_args()
     print(f'Arguments: {args}')
 
@@ -28,23 +28,23 @@ def main(args):
     print("Registering ", args.model_name)
 
     # Load model
-    model = mlflow.sklearn.load_model(args._______)  # Load the model from model_path
+    model = mlflow.sklearn.load_model(args.model_path)  # Load the model from model_path
 
     # Log model using mlflow
-    mlflow.sklearn._______(model, args._______)  # Log the model using with model_name
+    mlflow.sklearn.log_model(model, args.model_name)  # Log the model using with model_name
 
     # Register logged model using mlflow
     run_id = mlflow.active_run().info.run_id
     model_uri = f'runs:/{run_id}/{args.model_name}'
-    mlflow_model = mlflow.__________(model_uri, args._______)  # register the model with model_uri and model_name
-    model_version = mlflow_model._______  # Get the version of the registered model
+    mlflow_model = mlflow.register_model(model_uri, args.model_name)  # register the model with model_uri and model_name
+    model_version = mlflow_model.version  # Get the version of the registered model
 
     # Write model info
     print("Writing JSON")
     model_info = {"id": f"{args.model_name}:{model_version}"}
-    output_path = os.path.join(args.model_info_output_path, "________.json")  # Specify the name of the JSON file (model_info.json)
+    output_path = os.path.join(args.model_info_output_path, "model_info.json")  # Specify the name of the JSON file (model_info.json)
     with open(output_path, "w") as of:
-        json._____(model_info, of)  # write model_info to the output file
+        json.dump(model_info, of)  # write model_info to the output file
 
 if __name__ == "__main__":
     
@@ -54,9 +54,9 @@ if __name__ == "__main__":
     args = parse_args()
     
     lines = [
-        f"Model name: {args.________}",
-        f"Model path: {args.________}",
-        f"Model info output path: {args.________}"
+        f"Model name: {args.model_name}",
+        f"Model path: {args.model_path}",
+        f"Model info output path: {args.model_info_output_path}"
     ]
 
     for line in lines:
