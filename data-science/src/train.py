939fa38ae6369af0bc29b0483b6ba1b4314b4177
Swati Baikar <32398290+swatibaikar01@users.noreply.github.com>
2025-02-24 16:22:19 -0600

    Update train.py

--- data-science/src/train.py
+++ data-science/src/train.py
@@ -36,10 +36,10 @@ def main(args):
     test_df = pd.read_csv(Path(args.test_data) / "test_data.csv")
 
     # Split the data into ______(X) and ______(y) 
-    y_train = train_df['Price']  # Specify the target column
-    X_train = train_df.drop(columns=['Price'])
-    y_test = test_df['Price']
-    X_test = test_df.drop(columns=['Price'])
+    y_train = train_df['price']  # Specify the target column
+    X_train = train_df.drop(columns=['price'])
+    y_test = test_df['price']
+    X_test = test_df.drop(columns=['price'])
 
     # Initialize and train a RandomForest Regressor
     model = RandomForestRegressor(n_estimators=args.n_estimators, max_depth=args.max_depth, random_state=42)  # Provide the arguments for RandomForestRegressor
